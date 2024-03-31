0e9921a4728ed0c82ca667e1cb94489bf20936bb
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2024-03-31 14:55:18 +0400

    Implemented routes and fetcted data with react router 'loader', display loading indicator with useNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu />, loader: menuLoader },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <Order /> },
+      { path: "/order/:orderId", element: <CreateOrder /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Pizza app</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
