3545c44e682c95c08531bbf8f433505a0bf64738
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2024-04-06 22:23:51 +0400

    added tailwind css

---
+++ src/ui/Button.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to, type }) {
+  const base = `inline-block text-sm rounded-full bg-yellow-400  font-semibold
+  uppercase tracking-wide text-stone-800 transition-colorsduration-300
+   hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-nonefocus:ring
+    focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed`;
+
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' py-2 px-4 md:px-5 md:py-2.5 text-sm',
+    secondary: ` inline-block rounded-full bg-white-400 border-2  font-semibold
+    uppercase tracking-wide text-stone-800 transition-colorsduration-300
+     hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-nonefocus:ring
+      focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed
+       px-4 py-2.5 md:px-6 md:py-3.5`,
+  };
+
+  if (to)
+    return (
+      <Link className={styles[type]} to={to}>
+        {children}
+      </Link>
+    );
+  return (
+    <div className={styles[type]} disabled={disabled}>
+      {children}
+    </div>
+  );
+}
+
+export default Button;
